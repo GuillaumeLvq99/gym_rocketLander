d39eac798cf61ca168bcce5fb97eab8845466f89
Maxime Plocharski <maxime.plocharski@capgemini.com>
2023-12-01 13:05:29 +0100

    set break leg on speed condition

--- gym/envs/box2d/rocket_lander.py
+++ gym/envs/box2d/rocket_lander.py
@@ -487,11 +487,11 @@ class RocketLander(gym.Env):
         groundcontact = self.legs[0].ground_contact or self.legs[1].ground_contact
         y_abs_speed = vel_l[1] * np.sin(angle)
         brokenleg = False
-        brokenleg = (
-            self.legs[0].joint.angle < 0 or self.legs[1].joint.angle > -0
-        ) and groundcontact
-        # if groundcontact and abs(y_abs_speed) > self.speed_threshold:
-        #     brokenleg = True
+        #brokenleg = (
+        #    self.legs[0].joint.angle < 0 or self.legs[1].joint.angle > -0
+        #) and groundcontact
+        if groundcontact and abs(y_abs_speed) > self.speed_threshold:
+             brokenleg = True
         outside = abs(pos.x - W / 2) > W / 2 or pos.y > H
 
         fuelcost = 0.1 * (0 * (self.power + abs(self.force_dir))) / FPS
