9df512efaf07a3d2e83914a6b551cd5b7494cfc5
Maxime Plocharski <maxime.plocharski@capgemini.com>
2023-12-12 02:11:06 +0100

    modified reward

--- gym/envs/box2d/rocket_lander.py
+++ gym/envs/box2d/rocket_lander.py
@@ -557,11 +557,10 @@ class RocketLander(gym.Env):
                 if outside:
                     reward -= 10000
                 else:
-                    print('not out')
-                    reward -= max(100,10000*((self.total_fuel/700)+abs(speed)+abs(pos.x)))
+                    print(pos.x)
+                    reward -= max(100,1000*((self.total_fuel/700)+abs(speed)+abs(pos.x)))
 
             else:
-                print("good")
                 reward += max(100,10000*((self.total_fuel/700)+1-abs(pos.x)))
 
         reward = reward/1000
