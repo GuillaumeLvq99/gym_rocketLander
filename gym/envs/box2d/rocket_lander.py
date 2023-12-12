e3be0e14efc070f97aa4ca09e805a253baeb9b8f
Maxime Plocharski <maxime.plocharski@capgemini.com>
2023-12-12 15:53:42 +0100

    modified reward

--- gym/envs/box2d/rocket_lander.py
+++ gym/envs/box2d/rocket_lander.py
@@ -497,7 +497,7 @@ class RocketLander(gym.Env):
             if groundcontact and abs(y_abs_speed) > self.speed_threshold-0.01*self.level_number:
                 brokenleg = True
 
-        outside = abs(pos.x - W / 2) > 200 or pos.y > H
+        outside = abs(pos.x - W / 2) > 250 or pos.y > H
 
         #fuelcost = 0.1 * (self.power + abs(self.force_dir)) / FPS
         fuelcost = self.power
